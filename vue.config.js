7b0408283aea15b5422485b8b8a106050dde74a8
Alex Ochoa <alexx1708@gmail.com>
2021-08-15 03:31:27 -0600

    fix prod route

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,5 @@ module.exports = {
       enableEngine: false,
     },
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/hello-world/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/chat-room-frontend/" : "/",
 };
